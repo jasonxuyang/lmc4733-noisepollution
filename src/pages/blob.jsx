1d37023398dcf49ef16146caba10a901e86a0ea8
jasonxuyang <jasonxuyang@gmail.com>
2023-02-28 01:33:06 -0500

    chore: code cleanup

--- src/pages/blob.jsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const Blob = dynamic(() => import('@/components/canvas/Blob'), { ssr: false })
-
-export default function BlobPage() {}
-
-Page.canvas = () => <Blob route='/' position-y={-0.75} />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,10 @@
-import BlobPage from './blob'
+import dynamic from 'next/dynamic'
 
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
 // https://github.com/pmndrs/react-three-next/issues/49
-export default function HomePage(props) {
-  return BlobPage
-}
+const Blob = dynamic(() => import('@/components/canvas/Blob'), { ssr: false })
+
+export default function HomePage() {}
+HomePage.canvas = () => <Blob route='/' position-y={-0.75} />
